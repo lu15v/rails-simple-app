887f306bf136c9a6dcac9bab48e5371bf4fd01f8
lu15v <lu15bytes@gmail.com>
2018-01-25 22:58:35 -0600

    ADDED: model messages

--- decoder/db/schema.rb
+++ decoder/db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180126045741) do
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "encrypted"
+    t.string   "decoded"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
